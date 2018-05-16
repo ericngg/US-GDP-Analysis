adcb45cdc6f705ba7a395d9e693a0061d7ef939a
Ryan Chiu <ryanchiu@uw.edu>
2018-05-16 14:36:58 -0700

    organize data

--- analysis.R
+++ analysis.R
@@ -5,8 +5,15 @@ GDP_data <- read.csv(
   file = "./data/gdpstate.csv",
   stringsAsFactors = FALSE
   )
-
 #select columns of interest
+GDP_data <- GDP_data[-c(4681:5403), ]
 GDP_data_interest <- GDP_data %>%
-  select(GeoName, Description)
-  
+  select(GeoName, Description, starts_with("X"))
+
+# all industry data
+all_industry <- GDP_data_interest %>%
+  filter(Description == "All industry total")
+
+# Nationwide data
+nation_data <- GDP_data_interest %>%
+  filter(GeoName == "United States")
