c7194fcaa53b20bde958f44aaa530200813adb21
Andrew <syw1003@uw.edu>
2018-05-21 20:17:37 -0700

    andrew's draft

--- trend data.R
+++ trend data.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(plotly)
+library(snakecase)
 library(ggplot2)
 library(leaflet)
 
@@ -9,29 +10,44 @@ GDP_data <- read.csv(
   stringsAsFactors = FALSE
   )
 
-#select columns of interest
-GDP_data_interest <- GDP_data %>%
-  select(GeoName, Description)
-
-# Map data
-map_data <- GDP_data %>%
-  filter(Description == "All industry total") %>%
-  select(GeoName, X2017)
+# Industries of interest
+top_list <- c(1, 12, 35, 45, 50, 68, 82)
 
 # Trend data
 trend_data <- GDP_data %>%
   filter(GeoName == "United States",
-         Description == "All industry total") %>%
-  select(paste0("X", 1997:2017)) %>%
+         IndustryId %in% top_list) %>%
+  select(Description, paste0("X", 1997:2017)) %>%
   t() %>%
-  data.frame() %>%
-  mutate(year = c(1997:2017))
-colnames(trend_data) <- c("GDP", "Year")
-trend_data$GDP <- as.numeric(as.character(trend_data$GDP))
+  data.frame()
+colnames(trend_data) <- to_snake_case(unlist(trend_data["Description",]))
+trend_data <- trend_data[!row.names(trend_data)=="Description",]
+rownames(trend_data) <- 1997:2017
+trend_data <- trend_data %>%
+  mutate(year = row.names(trend_data))
+
+formatting <- function(x){
+  as.numeric(as.character(x))
+}
+trend_data[]<- lapply(trend_data[], formatting)
 
 # Trend chart
-trend_chart <- plot_ly(trend_data, x = ~Year, y = ~GDP,
+us_trend_chart <- plot_ly(trend_data, x= ~year, y = ~all_industry_total,
+                       name = "U.S. GDP",
              type = "scatter", mode = "lines+markers") %>%
   layout(title = "U.S. GDP Trend (1997-2017)",
          xaxis = list(title = "Year"),
          yaxis = list(title = "GDP (in millions)"))
+
+industry_trend_chart <- plot_ly(trend_data, x= ~year, y = ~manufacturing,
+                          name = "Manufacturing",
+                          type = "scatter", mode = "lines+markers") %>%
+  layout(title = "U.S. GDP Trend by industry (1997-2017)",
+         xaxis = list(title = "Year"),
+         yaxis = list(title = "GDP (in millions)")) %>%
+  add_trace(x= ~year, y = ~information,
+            name = "Information", mode = "lines+markers") %>%
+  add_trace(x= ~year, y = ~retail_trade,
+            name = "Retail", mode = "lines+markers") %>%
+  add_trace(x= ~year, y = ~finance_insurance_real_estate_rental_and_leasing,
+            name = "Finance", mode = "lines+markers")
